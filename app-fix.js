c5a5570800f7810e864252c77fd72f6d7f3bbeb8
mertcankose <mertcankose97@outlook.com>
2020-08-28 17:14:59 +0300

    optimization

--- app-fix.js
+++ app-fix.js
@@ -4,6 +4,17 @@ new Vue({
     startGameControl: false,
     healthMe: 100,
     healthMonster: 100,
+    attackMultiple: 10,
+    specialAttackMultiple: 20,
+    monsterAttackMultiple: 15,
+    firstAidMultiple: 20,
+    logText: {
+      personAttack: "Oyuncu Atağı!",
+      personSpecialAttack: "Özel Oyuncu Atağı",
+      monsterAttack: "Monster Atağı!",
+      firstAid: "İlk YARDIM",
+      giveUp: "Oyuncu Pes Etti!",
+    },
     logs: [],
   },
   methods: {
@@ -12,30 +23,34 @@ new Vue({
     },
 
     attack: function () {
-      var personPowerAttack = Math.floor(Math.random() * 17) + 8;
+      var personPowerAttack = Math.floor(Math.random() * this.attackMultiple);
       this.healthMonster -= personPowerAttack;
       this.monsterAttack();
       this.addToLog({
         turn: "Person",
-        text: "Oyuncu Atağı! (" + personPowerAttack + ")",
+        text: this.logText.personAttack + "(" + personPowerAttack + ")",
       });
     },
 
     specialAttack: function () {
-      var personPowerAttack = Math.floor(Math.random() * 22) + 10;
+      var personPowerAttack =
+        Math.floor(Math.random() * this.specialAttackMultiple) + 10;
       this.healthMonster -= personPowerAttack;
       this.monsterAttack();
       this.addToLog({
         turn: "Person",
-        text: "Özel Oyuncu Atağı! (" + personPowerAttack + ")",
+        text: this.logText.personSpecialAttack + "(" + personPowerAttack + ")",
       });
     },
 
     firstAid: function () {
-      var aid = Math.floor(Math.random() * 25) + 15;
+      var aid = Math.floor(Math.random() * this.firstAidMultiple);
       this.healthMe += aid;
       this.monsterAttack();
-      this.addToLog({ turn: "Person", text: "İlk Yardım! (" + aid + ")" });
+      this.addToLog({
+        turn: "Person",
+        text: this.logText.firstAid + "(" + aid + ")",
+      });
     },
 
     giveUp: function () {
@@ -44,15 +59,17 @@ new Vue({
         this.startGameControl = false;
         this.healthMe = 0;
       }
-      this.addToLog({ turn: "Person", text: "Oyuncu Pes Etti!" });
+      this.addToLog({ turn: "Person", text: this.logText.giveUp });
     },
 
     monsterAttack: function () {
-      var monsterPowerAttack = Math.floor(Math.random() * 17) + 8;
+      var monsterPowerAttack = Math.floor(
+        Math.random() * this.monsterAttackMultiple
+      );
       this.healthMe -= monsterPowerAttack;
       this.addToLog({
         turn: "Monster",
-        text: "Canavar Atağı! (" + monsterPowerAttack + ")",
+        text: this.logText.monsterAttack + "(" + monsterPowerAttack + ")",
       });
     },
 
@@ -84,6 +101,18 @@ new Vue({
           this.logs = [];
         }
       }
+    },
+  },
+  computed: {
+    playerProgress: function () {
+      return {
+        width: this.healthMe + "%",
+      };
+    },
+    monsterProgress: function () {
+      return {
+        width: this.healthMonster + "%",
+      };
     },
   },
 });
